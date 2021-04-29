01e4506cec93e2c5bf2411cabe794d78c02d6a49
lfrancisco <franciscoaglima@gmail.com>
2021-04-29 12:29:29 -0300

    2o Etapa - Configuração API- com Json

--- lib/API.dart
+++ lib/API.dart
@@ -1,12 +1,17 @@
 import 'package:http/http.dart' as http;
+
 import 'dart:convert';
 import 'package:movienews_app/movie.dart';
 
 class API {
 
+  Map<String,String> queryParameters;
+
   Future<Movie> fetchMovie(int num) async {
  
-   final response = await http.get(Uri.https("api.themovie.org", "/3/movir/upcoming"));
+    final url =  Uri.https("api.themoviedb.org", "/3/movie/upcoming", queryParameters = {"api_key":"a5c05fb630c9b7dc560033345fa13e",});
+
+   final response = await http.get(url);
   
    if (response.statusCode == 200) {
       return Movie.fromJson(jsonDecode(response.body));
