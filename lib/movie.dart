f2d9c45588f3481a958e79676bb36c0618b0623f
lfrancisco <franciscoaglima@gmail.com>
2021-05-01 16:03:10 -0300

    4o Etapa -Em Depuração - API MovieNews

--- lib/movie.dart
+++ lib/movie.dart
@@ -1,17 +1,18 @@
 class Movie{
-  String titulo;
-  String urlPath;
-  String data;
-  // List<Ability> abilities;
-  
-  int ord = 0; // weight
-  int index = 0; // height
-  int peso = 0; // weight
 
-  Movie.fromJson(Map<String,dynamic> json){
-   titulo = json["title"];
-   urlPath = json["sprites"]["front_default"];
+ List<Detalhe> details;
 
-  }
 }
 
+class Detalhe {
+  String titulo;
+  String postPath;
+  String data;
+
+  Detalhe.fromJson(Map<String,dynamic> json) {
+    titulo = json["title"];
+   postPath = json["poster_path"];
+   data = json["release_date"];
+  }
+
+}
--- lib/movie_view.dart
+++ lib/movie_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:movienews_app/movie.dart';
 import 'movie_controller.dart';
 
 class MovieView extends StatefulWidget {
@@ -22,18 +23,53 @@ class _MovieViewState extends State<MovieView> {
                   child: Stack(
                     children: [
                       Container(
-                        color:Colors.white,
                         height: 820,
                         width: 400,
-                        padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                      ),
-                      Center(
-                        child: Container(
-                        height: 730,
-                        width: 390,  
-                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(50),color: Colors.amber,),                      
-                        ),
-                      ),
+                        color: Colors.pink,
+                   // decoration: BoxDecoration(borderRadius: BorderRadius.circular(50),color: Colors.amber,),
+                    ),
+             Center(child: Column(mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      FutureBuilder<Movie>(future: controller.movie,builder: (context,snapshot) {
+                           if (snapshot.connectionState != ConnectionState.done) {return CircularProgressIndicator();}
+                            if (snapshot.hasData) {
+                              return Column(  
+                                children: [
+                                  Text("Código",
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.black),
+                                    ),                                  
+                                  Container( 
+                                    color: Colors.yellow,
+                                    height: 80,
+                                    width: 280,
+                                   child: ListView.builder(
+                                     itemCount: 2,
+                                      itemBuilder: (
+                                       BuildContext context ,int index) {
+                                        return ListTile(
+                                          title: Text(snapshot.data.details[index].titulo),
+                                          );
+                                        }
+                                       ),
+                                      ),
+                                  Text(" ",style: TextStyle(fontSize: 10,color: Colors.black)),                                  
+                                ],
+                              );
+                            } else if (snapshot.hasError) {
+                              return Text(snapshot.error,
+                              style: TextStyle(
+                                  fontSize: 18, 
+                                  color: Colors.redAccent),
+                                  );
+                                }
+                            return Container();
+                          },
+                         ),
+                      ElevatedButton(onPressed: () {setState(() {controller.loadMovie();});}, child: Text("Load Filmes"),)
+                    ],),),
+
 //                     Center(child: Container(height: 640,width: 320,
 //                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(50),color: Colors.blue,), ),),                      
                     ],
